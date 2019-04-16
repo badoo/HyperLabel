1ce8a1e70ef0801f9191e68ccb673eaa0f49c0dd
Victor Shamanov <wiruzx@gmail.com>
2019-04-16 16:23:01 +0100

    Remove storyboard

--- HyperLabelDemo/HyperLabelDemo/AppDelegate.swift
+++ HyperLabelDemo/HyperLabelDemo/AppDelegate.swift
@@ -29,7 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 }
